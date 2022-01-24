fab08a24116d39bc696505569d2e273e73740de5
angelawade <angelafariasgomez@gmail.com>
2022-01-24 20:40:34 -0300

    agregue un fuction al js

--- java.js
+++ java.js
@@ -1,6 +1,27 @@
 let correoElectronico = 'desafio1@desafio1';
 let email = prompt ('Ingresar dirección de correo electrónico');
 let contraseña = prompt ('Ingresar contraseña');
+let nombreYapellido = prompt ('Ingresar nombre y apellido');
+let correoCorreo = prompt('Ingresar correo electrónico');
+let mensaje = prompt ('Ingresar mensaje');
+
+
+function solicitarDato() {
+    let dato = prompt('Campo obligatorio');
+    alert (dato)
+}
+
+if (nombreYapellido == "") {
+solicitarDato(nombreYapellido);
+} 
+
+if (correoCorreo == "") {
+    solicitarDato(correoCorreo);
+}
+
+if (mensaje == "") {
+    solicitarDato(mensaje);
+}
 
 if (email == "") {
     alert("No ingresaste dirección de correo electrónico");
@@ -15,3 +36,4 @@ while( email != correoElectronico) {
     email = prompt ('Ingresar otro dato');
 
 }
+
