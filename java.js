32405f0c061f5f59700e84e52ba88956ed452dbc
angelawade <angelafariasgomez@gmail.com>
2022-01-31 23:11:09 -0300

    agregue arrays

--- java.js
+++ java.js
@@ -37,3 +37,20 @@ while( email != correoElectronico) {
 
 }
 
+
+
+
+//ARRAYS
+const regnum = [1, 2, 3, 4, 5, 6];
+const capitulos = ['Capítulo 1', 'Capítulo 2', 'Capítulo 3', 'Capítulo 4', 'Capítulo 5', 'Capítulo 6'];
+
+
+console.log (regnum[0] + capitulos[0]);
+console.log (regnum[1] + capitulos[1]);
+console.log (regnum[2] + capitulos[2]);
+console.log (regnum[3] + capitulos[3]);
+console.log (regnum[4] + capitulos[4]);
+console.log (regnum[5] + capitulos[5]);
+
+console.log(regnum.length);
+console.log(capitulos.length);
